7dfde6c713e25db81a8a3e434e97844eb10d28cb
maria <mcmseves@gmail.com>
2016-06-04 16:29:35 -0300

    mapa com endereco de alunos implementado

--- app/src/main/java/br/com/caelum/cadastro/util/Localizador.java
+++ app/src/main/java/br/com/caelum/cadastro/util/Localizador.java
@@ -1,7 +1,39 @@
 package br.com.caelum.cadastro.util;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by android6040 on 04/06/16.
  */
 public class Localizador {
+    private Geocoder geo;
+
+    public Localizador(Context ctx){
+        geo = new Geocoder(ctx);
+    }
+
+
+    public LatLng getCoordenada(String endereco){
+        try {
+            List<Address> listaEnderecos;
+            listaEnderecos = geo.getFromLocationName(endereco,1);
+            if(!listaEnderecos.isEmpty()){
+                Address address = listaEnderecos.get(0);
+                return new LatLng(address.getLatitude(), address.getLongitude());
+            }
+            else {
+                return null;
+            }
+        }
+        catch (IOException e){
+            return null;
+        }
+    }
 }
--- app/src/main/java/fragment/MapaFragment.java
+++ app/src/main/java/fragment/MapaFragment.java
@@ -1,11 +1,24 @@
 package fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import br.com.caelum.cadastro.modelo.Aluno;
+import br.com.caelum.cadastro.modelo.AlunoDAO;
+import br.com.caelum.cadastro.util.Localizador;
 
 /**
  * Created by android6040 on 04/06/16.
@@ -14,5 +27,42 @@ public class MapaFragment extends SupportMapFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getMapAsync(new OnMapReadyCallback(){
+            @Override
+            public void onMapReady(GoogleMap googleMap){
+                Localizador localizador = new Localizador(getActivity());
+                LatLng local = localizador.getCoordenada("Rua Vergueiro 3185 Vila Mariana");
+                centralizaNo(local, googleMap);
+
+
+                AlunoDAO dao = new AlunoDAO(getActivity());
+                List<Aluno> alunos = dao.getLista();
+
+                for (Aluno aluno : alunos){
+                    LatLng coordenada = localizador.getCoordenada(aluno.getEndereco());
+
+                    if(coordenada!=null){
+                        MarkerOptions marcador = new MarkerOptions()
+                                .position(coordenada)
+                                .title(aluno.getNome())
+                                .snippet(aluno.getEndereco());
+                        googleMap.addMarker(marcador);
+
+                    }
+                }
+            }
+        }
+        );
+
+    }
+
+    private void centralizaNo(LatLng local, GoogleMap mapa) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(local,6));
     }
 }
